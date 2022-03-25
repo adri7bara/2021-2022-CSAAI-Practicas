e37f089816ed7b645774c4d3080229a23117d238
Adrian Barahona <a.barahona.2016@alumnos.urjc.es>
2022-03-25 16:28:39 +0100

    calculadora final

---
+++ P2/cacluladora_readme.md
@@ -0,0 +1,13 @@
+La calculadora cuenta con botones numéricos del 0 al 9. También permite la introducción de números decimales ya que cuenta con el boton del punto ".". 
+Las operaciones que permite realizar son suma, resta, multiplicación, división y potencia. 
+La calculadora cuenta con una pantalla o display donde se observan los números y operaciones seleccionadas.
+Dispone también con dos opciones de borrado:
+    - El botón "DEL" que permite borrar el último número introducido en nuestro display. 
+    - El botón "AC" que permite resetear la calculadora a su estado inicial mostrando en el display el número 0.
+
+Se permite realizar operaciones del siguiente tipo: 
+    - <Numero> <Operador> <Número>
+Es decir, las operaciones siempre serán sólo entre dos números. 
+Así, la expresión "1+2+3" no es válida. 
+Para calcularla habrá que hacer primero "1+2" y darle al igual. 
+A este resultado ahora ya le podemos sumar el 3. 
--- P2/calculadora.js
+++ P2/calculadora.js
@@ -1,9 +1,76 @@
 console.log("Ejecutando JS...");
 
-//-- 4 estados de la calculadora
+//-- Estados de la calculadora
 const ESTADO = {
     INIT: 0,
     OP1: 1,
     OPERATION: 2,
     OP2: 3
-  }
+}
+
+//-- Leer y almacenar los elementos de entrada
+display = document.getElementById("display")
+digitos = document.getElementsByClassName("digito") 
+operadores = document.getElementsByClassName("operador") 
+igual = document.getElementById("igual")
+AC = document.getElementById("AC")
+borrar = document.getElementById("delete")
+
+//-- Al principio la calculadora se encuentra en estado INIT
+let estadocalculadora = ESTADO.INIT;
+
+ //-- Función manejadora de los números introducidos
+function digito(ev){
+    if (estadocalculadora == ESTADO.INIT) {
+        display.innerHTML = ev.target.value;
+        estadocalculadora = ESTADO.OP1;
+    } else if (estadocalculadora == ESTADO.OP1){
+        display.innerHTML += ev.target.value;
+    } else if (estadocalculadora == ESTADO.OPERATION){
+        display.innerHTML += ev.target.value;
+        estadocalculadora = ESTADO.OP2;
+    } else if (estadocalculadora == ESTADO.OP2){
+        display.innerHTML += ev.target.value;
+    }
+}
+
+//-- Llamar a la función digito siempre que se introduzca un número
+for (let boton of digitos) {
+    boton.onclick = digito;
+}
+
+//-- Establecer la misma función de retrollamada
+//-- para todos los botones de tipo signo
+for (let boton of operadores) {
+    boton.onclick = (ev) => {
+      if(estadocalculadora == ESTADO.OP1){
+        display.innerHTML += ev.target.value;
+       estadocalculadora = ESTADO.OPERATION;
+      }
+    }
+}
+
+//-- Evaluar la expresión introducida
+igual.onclick = () =>  {
+    if(estadocalculadora == ESTADO.OP1 ||  estadocalculadora == ESTADO.OP2){
+    display.innerHTML = eval(display.innerHTML);
+    estadocalculadora = ESTADO.OP1;
+    }
+}
+  
+//-- Borrar último elemento introducido
+borrar.onclick = () => {
+    display.innerHTML = display.innerHTML.slice(0,-1);
+    if (display.innerHTML == 0){
+      display.innerHTML = "0";
+      console.log("Delete");
+      estadocalculadora = ESTADO.INIT;
+    }
+}
+
+//-- Volver al estado inicial con el boton AC
+AC.onclick = () => {
+    display.innerHTML = "0";
+    console.log("AC");
+    estadocalculadora = ESTADO.INIT;
+}
